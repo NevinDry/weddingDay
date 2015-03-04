ec8cece65bff0073fcf6263c7b93d31ef1f2f710
Nevin <Nevin@Nevin-PC>
2015-03-04 23:45:46 +0100

    Here we are, the end, process OK

---
+++ process.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_GET["select"]))
+{
+	$select = $_GET["select"];
+}
+	
+if(isset($_GET["nom"]))
+{
+	$nom = $_GET["nom"];
+}
+
+$varForCsv = $nom." --- ".$select."\r\n";
+
+$fichierUser = 'invitation.txt';
+
+if(!$fp = fopen($fichierUser, 'a')){
+	exit;
+} else {
+	fwrite($fp, $varForCsv);
+	fclose($fp);
+}
