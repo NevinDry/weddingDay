58d04bcad1e27627206899050321e5e0194170e2
Nevin <Nevin@Nevin-PC>
2015-03-05 00:21:59 +0100

    Test sécu

--- process.php
+++ process.php
@@ -7,15 +7,15 @@ if(isset($_GET["select"]))
 if(isset($_GET["nom"]))
 {
 	$nom = $_GET["nom"];
+	$varForCsv = $nom." --- ".$select."\r\n";
+	
+	$fichierUser = 'file/invitation.txt';
+	
+	if(!$fp = fopen($fichierUser, 'a')){
+		exit;
+	} else {
+		fwrite($fp, $varForCsv);
+		fclose($fp);
+	}
 }
 
-$varForCsv = $nom." --- ".$select."\r\n";
-
-$fichierUser = 'invitation.txt';
-
-if(!$fp = fopen($fichierUser, 'a')){
-	exit;
-} else {
-	fwrite($fp, $varForCsv);
-	fclose($fp);
-}
